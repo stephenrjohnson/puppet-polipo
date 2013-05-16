27e14f701e5688896c44a02632faf3ba6c215ca9
Stephen <stephen@puppetlabs.com>
2013-05-17 09:54:48 +1200

    Add forbidden support

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,6 +24,7 @@
 #   allowedclients => '127.0.0.1',  # default: any   | any static manual dhcp bootp ppp wvdial
 #   proxyname      => 'foobar',     # default: $hostname
 #   sharedcache    => 'false'
+#   forbiden       => ['example.com','^http://[^/]*counter\.com']
 #
 # When a file called forbidden-$fqdn is present it will
 # be used to override the default forbidden list.
@@ -33,7 +34,8 @@ class polipo (
   $proxyaddress   = '::0',
   $allowedclients = '127.0.0.1',
   $proxyname      = "\"${::hostname}\"",
-  $sharedcache    = 'false'
+  $sharedcache    = false,
+  $forbiden       = [],
 ) inherits polipo::params {
   package {'polipo':
     ensure => installed,
@@ -42,46 +44,47 @@ class polipo (
 
   File{
     require => Package['polipo'],
-    }
-
-    file {$polipo::params::polipo_confdir:
-      ensure => directory,
-    }
-
-    file{'polipoconf':
-      ensure  => file,
-      path    => "${polipo::params::polipo_confdir}/config",
-      content => template ('polipo/config.erb'),
-    }
-
-    file{"${polipo::params::polipo_confdir}/forbidden":
-      ensure   => file,
-      content  => template('polipo/forbidden.erb'),
-    }
-
-    file{"${polipo::params::polipo_confdir}/options":
-      ensure  => file,
-      content => template ('polipo/options.erb');
-    }
-
-    file{$polipo::params::polipo_cachedir:
-      ensure => directory,
-    }
-
-    cron {'Purge Polipo cache every two weeks':
-      command  => "kill -USR1 $(cat ${polipo::params::polipo_pidfile}); sleep 1; polipo -x; kill -USR2 $(cat ${polipo::params::polipo_pidfile})",
-      user     => 'root',
-      month    => '*',
-      monthday => ['1', '14'],
-      hour     => '0',
-      minute   => '0',
-    }
-
-    service {$polipo::params::polipo_name:
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      hasstatus  => false,
-      subscribe  => File['polipoconf'],
-    }
+    notify  => Service['polipo'],
   }
+
+  file {$polipo::params::polipo_confdir:
+    ensure => directory,
+  }
+
+  file{'polipoconf':
+    ensure  => file,
+    path    => "${polipo::params::polipo_confdir}/config",
+    content => template ('polipo/config.erb'),
+  }
+
+  file{"${polipo::params::polipo_confdir}/forbidden":
+    ensure   => file,
+    content  => template('polipo/forbidden.erb'),
+  }
+
+  file{"${polipo::params::polipo_confdir}/options":
+    ensure  => file,
+    content => template ('polipo/options.erb');
+  }
+
+  file{$polipo::params::polipo_cachedir:
+    ensure => directory,
+  }
+
+  cron {'Purge Polipo cache every two weeks':
+    command  => "kill -USR1 $(cat ${polipo::params::polipo_pidfile}); sleep 1; polipo -x; kill -USR2 $(cat ${polipo::params::polipo_pidfile})",
+    user     => 'root',
+    month    => '*',
+    monthday => ['1', '14'],
+    hour     => '0',
+    minute   => '0',
+  }
+
+  service {'polipo':
+    ensure     => running,
+    name       => $polipo::params::polipo_name,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => false,
+  }
+}
